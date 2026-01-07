b5fd5adeddc93e2a727a329163f7a202aae540d1
DarcherDev <daniel_aristizabal@outlook.com>
2026-01-07 16:23:26 -0500

    Add new routes with custom HTTP status codes
    Added three new GET endpoints: /notFound (404), /erro (500), and /new (201), each returning a simple message with the specified HTTP status code. Also imported HttpCode from @nestjs/common to support these changes.

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, Req, Res,HttpCode } from '@nestjs/common';
 import type { Request, Response } from 'express';
 
 @Controller()
@@ -9,4 +9,23 @@ export class HelloController {
       message: 'aaaaa',
     });
   }
+
+  @Get('/notFound')
+  @HttpCode(404)
+  notFoundPage(){
+    return '404 not found'
+  }
+
+  @Get('/erro')
+  @HttpCode(500)
+  errorPage(){
+    return 'Error Route!!!'
+  }
+
+  @Get('/new')
+  @HttpCode(201)
+  somethingNew(){
+    return 'somethingNew'
+  }
+
 }
