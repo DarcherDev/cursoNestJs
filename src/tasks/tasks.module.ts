70ab2a1c775d0fc2a59ee438b0d31e29ac3b049d
DarcherDev <daniel_aristizabal@outlook.com>
2026-01-08 20:32:05 -0500

    Add logger and auth middlewares to tasks module
    Introduced LoggerMiddleware and AuthMiddleware for the tasks module. LoggerMiddleware logs request URLs for GET and POST requests to 'tasks', while AuthMiddleware enforces authorization on all 'tasks' routes. Also added a basic logger middleware and its test for the users module.

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,12 +1,29 @@
-import {  Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TasksController } from './controllers/tasks.controller';
 import { TasksService } from './services/tasks.service';
+import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
+import { AuthMiddleware } from './middlewares/auth/auth.middleware';
 
 @Module({
-    controllers:[TasksController],
-    providers: [TasksService]
+  controllers: [TasksController],
+  providers: [TasksService],
 })
+export class TasksModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
 
-export class TasksModule {
-
- }
+    //consumo de rutas para rutas expecificas
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        {
+          path: 'tasks',
+          method: RequestMethod.GET,
+        },
+        {
+          path: 'tasks',
+          method: RequestMethod.POST,
+        }
+      )
+      .apply(AuthMiddleware).forRoutes('tasks') //consumo para todas las rutas del modulo
+  }
+}
